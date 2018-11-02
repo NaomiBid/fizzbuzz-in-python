b9c15aaa0047828b0491135c714a4be4e52f086e
NaomiBid <naomi0bid@gmail.com>
2018-11-02 11:40:23 +0000

    wrote test for 'fizz' and passed it

--- pairing_week_5/fizzbuzz_in_python/fizzbuzz.py
+++ pairing_week_5/fizzbuzz_in_python/fizzbuzz.py
@@ -1,2 +1,5 @@
 def FizzBuzz(number):
+    if number % 3 == 0:
+        return "fizz"
+
     return number
--- pairing_week_5/fizzbuzz_in_python/test_fizzbuzz.py
+++ pairing_week_5/fizzbuzz_in_python/test_fizzbuzz.py
@@ -3,9 +3,14 @@ from fizzbuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_simple_should_return_the_number(self):
-		      self.assertEqual(FizzBuzz(1), 1)
+	def test_simple_should_return_the_number(self):
+		self.assertEqual(FizzBuzz(1), 1)
+		self.assertEqual(FizzBuzz(17), 17)
+		self.assertEqual(FizzBuzz(23), 23)
 
+	def test_multiple_3_should_return_fizz(self):
+		self.assertEqual(FizzBuzz(3), "fizz")
+		self.assertEqual(FizzBuzz(9), "fizz")
 
 if __name__ == '__main__':
 	unittest.main()
